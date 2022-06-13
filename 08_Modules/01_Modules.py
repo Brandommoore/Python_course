91b7d1b4d405a78f4cda81132faea8565f5076f4
Brandom moore <brandommoore@gmail.com>
2022-06-13 21:09:37 +0200

    Modules_added

---
+++ 08_Modules/01_Modules.py
@@ -0,0 +1,10 @@
+#  Modules
+# - Son un archivo con extension .py .pyc o archivo escrito en C para CPython, que posee su propio espacio de nombres
+#	y que puede contener variables, funciones, clases e incluso otros modulos.
+# - Sirven para organizar y reutilizar el codigo
+# - Se crea haciendo unarchivo .py y guardandolo donde lo necesitemos
+
+from math_functions import *
+
+if __name__ == '__main__':
+	multiply(512, 8)
---
+++ 08_Modules/math_functions.py
@@ -0,0 +1,8 @@
+def sume(nb1, nb2):
+	print(f"The result is {nb1+nb2}")
+
+def reste(nb1, nb2):
+	print(f"The result is {nb1-nb2}")
+
+def multiply(nb1, nb2):
+	print(f"The result is {nb1*nb2}")
